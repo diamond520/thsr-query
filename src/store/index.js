580db83f42407a919c7bdd4f5769246663c87bf6
diamond Hung <diamond@dropbuy.com.tw>
2020-03-15 16:09:39 +0800

    ✨ Feat: use vuex to get stations list

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,29 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { getStations } from '@/api/thrs-api'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    stations: []
   },
   mutations: {
+    setStations (state, data) {
+      state.stations = data
+    }
   },
   actions: {
+    async getStations ({commit}) {
+      const res = await getStations()
+      commit('setStations', res)
+    }
+  },
+  getters: {
+    stations: state => state.stations.map(station => ({
+      text: station.StationName.Zh_tw,
+      id: station.StationID
+    }))
   },
   modules: {
   }
