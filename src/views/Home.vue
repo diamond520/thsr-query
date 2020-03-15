f453955d7e55c2310c9dfa2124c465f2d494781f
diamond Hung <diamond@dropbuy.com.tw>
2020-03-15 16:10:52 +0800

    ✨ Feat: main layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <el-container>
+    <!-- <el-page-header @back="goBack" content="详情页面">
+    </el-page-header> -->
+    <el-header>高鐵查詢系統</el-header>
+
+    <el-main>
+      <el-tabs type="border-card">
+        <el-tab-pane label="時間查詢">
+          <byTime></byTime>
+        </el-tab-pane>
+        <el-tab-pane label="班次停靠站查詢">停靠站</el-tab-pane>
+        <el-tab-pane label="車站座位查詢">角色管理</el-tab-pane>
+      </el-tabs>
+    </el-main>
+  </el-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+// import test from '@/components/test.vue'
+import byTime from '@/components/byTime.vue'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    // test,
+    byTime
+  },
+  methods: {
+    goBack() {
+      console.log('go back')
+    }
   }
 }
 </script>
+
+<style lang="scss">
+
+</style>
