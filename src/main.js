62731868aee6bb8c57aeb4e051c1367a7134c716
diamond Hung <diamond@dropbuy.com.tw>
2020-03-15 16:01:50 +0800

    🎨 Style: import normalize.css and global css

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'normalize.css'
+import './assets/style.scss'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
